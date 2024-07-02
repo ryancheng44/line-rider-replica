efc147add70ee79c95832de6946332e2880c70cd
ryancheng44 <ryancheng44@gmail.com>
2024-07-02 11:53:43 -0400

    Added Line Rider Replica project

---
+++ Line Rider Replica/Assets/Scripts/LineCreator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineCreator : MonoBehaviour
+{
+    public GameObject linePrefab;
+
+    Line activeLine;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject lineGameObject = Instantiate(linePrefab);
+            activeLine = lineGameObject.GetComponent<Line>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePosition);
+        }
+    }
+}
---
+++ Line Rider Replica/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Play"))
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+    }
+}
